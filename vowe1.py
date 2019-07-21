82965bd15e600d8aafe056904f69c0082d6d5f22
lokivmsl98 <53137000+lokivmsl98@users.noreply.github.com>
2019-07-21 09:37:34 -0700

    Update and rename vowe.py to vowe1.py

--- vowe1.py
+++ vowe1.py
@@ -1,7 +1,8 @@
 l=input()
-if(l=="a" or l=="A" or l=="e" or l=="E" or l=="i" or l=="I" or l=="o" or l=="O" or l=="u" or l=="U"):
+if(l=="a"or l=="A"or l=="e" or l=="E" or l=="i" or l=="I" or l=="o" or l=="O" or l=="u" or l=="U"):
   print("vowel")
 elif(l>="a" and l<="z") or (l>="A" and l<="Z"):
   print("Consonant")
 else:
   print("invalid")
+
