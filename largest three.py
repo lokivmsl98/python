f3d9d9e2dcc6918a9d599a1909d4846cc15b2822
lokivmsl98 <53137000+lokivmsl98@users.noreply.github.com>
2019-07-21 10:38:49 -0700

    Create largest three.py

---
+++ largest three.py
@@ -0,0 +1,10 @@
+l1=int(input())
+l2=int(input())
+l3=int(inpu())
+if(l1>l2) and (l1>l3):
+  print("Largest=l1")
+elif(l2>l1) and (l2>l3):
+  print("Largest=l2")
+else:
+  largest =l3
+  print("the Largest number is",Largest)
