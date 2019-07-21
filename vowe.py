0d73e33ff431bed1b126b083f0f79a45f3f68b8f
lokivmsl98 <53137000+lokivmsl98@users.noreply.github.com>
2019-07-21 09:32:45 -0700

    Update vowe.py

--- vowe.py
+++ vowe.py
@@ -2,6 +2,6 @@ l=input()
 if(l=="a" or l=="A" or l=="e" or l=="E" or l=="i" or l=="I" or l=="o" or l=="O" or l=="u" or l=="U"):
   print("vowel")
 elif(l>="a" and l<="z") or (l>="A" and l<="Z"):
-  print("consonant")
+  print("Consonant")
 else:
   print("invalid")
