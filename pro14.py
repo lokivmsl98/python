8f911fce05e617406bd08b0532b4bb4f634806d0
lokivmsl98 <53137000+lokivmsl98@users.noreply.github.com>
2019-08-04 09:16:02 -0700

    Create pro14.py

---
+++ pro14.py
@@ -0,0 +1,13 @@
+   
+a,b=map(int,input().split())
+l=list(map(int,input().split()))
+c=[]
+for i in range(0,b):
+    d = []
+    d=list(map(int,input().split()))
+    s = l[d[0]-1]
+    for j in range(min(d),max(d)):
+        s=s^l[j]
+    c.append(s)
+for i in range(0,len(c)):
+    print(c[i]) 
