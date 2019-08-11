d08b2ac6c45fc990d3113cbd48eda6d72f64b08b
lokivmsl98 <53137000+lokivmsl98@users.noreply.github.com>
2019-08-11 03:08:44 -0700

    Create hunter 13.py

---
+++ hunter 13.py
@@ -0,0 +1,28 @@
+class Stack:
+    def __init__(loki):
+        loki.items = []
+ 
+    def is_empty(loki):
+        return loki.items == []
+ 
+    def push(loki, data):
+        loki.items.append(data)
+ 
+    def pop(loki):
+        return loki.items.pop()
+ 
+ 
+s = Stack()
+text = input(' ')
+ 
+for character in text:
+    s.push(character)
+ 
+reversed_text = ''
+while not s.is_empty():
+    reversed_text = reversed_text + s.pop()
+ 
+if text == reversed_text:
+    print('yes')
+else:
+    print('no')
