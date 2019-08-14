9ecd743203d17827fb02804cd23684c86a157420
lokivmsl98 <53137000+lokivmsl98@users.noreply.github.com>
2019-08-14 00:52:20 -0700

    Create beg 52.py

---
+++ beg 52.py
@@ -0,0 +1,23 @@
+k=int(input())
+if k==1:
+    print('one')
+elif k==2:
+    print("Two")
+elif k==3:
+    print("Three")
+elif k==4:
+    print("Four")
+elif k==5:
+    print("Five")
+elif k==6:
+    print("Six")
+elif k==7:
+    print("Seven")
+elif k==8:
+    print("Eignt")
+elif k==9:
+    print("Nine")
+elif k==10:
+    print("Ten")
+else:
+    print("0")
